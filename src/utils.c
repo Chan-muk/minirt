9517599f510baf48bfcf597f9169af55735feae5
CM <jcm0623@naver.com>
2023-05-01 09:38:47 +0900

    ADD: ascii to double

---
+++ src/utils.c
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chajung <chajung@student.42seoul.k>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/01 08:57:27 by chajung           #+#    #+#             */
+/*   Updated: 2023/05/01 08:57:28 by chajung          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minirt.h"
+
+void	exit_with_str(const char *str, int exit_code)
+{
+	ft_putendl_fd((char *)str, STDERR_FILENO);
+	exit(exit_code);
+}
+
+static int	white_space_sign_check(char *str, int *index)
+{
+	int	sign;
+
+	sign = 1;
+	while ((9 <= str[*index] && str[*index] <= 13) || str[*index] == 32)
+		*index += 1;
+	if (str[*index] == '+' || str[*index] == '-')
+	{
+		if (str[*index] == '-')
+			sign = sign * (-1);
+		*index += 1;
+	}
+	return (sign);
+}
+
+static void	is_double(char *str)
+{
+	int	sign;
+	int	c;
+	int	dot;
+
+	sign = 0;
+	c = 0;
+	dot = 0;
+	while (str && *str)
+	{
+		if (*str == '+' || *str == '-')
+			sign++;
+		else if (*str == '.')
+			dot++;
+		else if (!ft_isdigit(*str) && ft_isascii(*str))
+			c++;
+		str++;
+	}
+	if (!str || sign > 1 || dot > 1 || c > 0)
+		exit_with_str("input right number", EXIT_FAILURE);
+}
+
+double	ascii_to_double(char *str)
+{
+	int		index;
+	int		sign;
+	int		decimal_count;
+	double	number;
+
+	index = 0;
+	decimal_count = 0;
+	number = 0.0;
+	is_double(str);
+	sign = white_space_sign_check(str, &index);
+	while (str[index] && ft_isdigit(str[index]))
+	{
+		number = 10.0 * number + (str[index] - '0');
+		index++;
+	}
+	if (str[index] == '.')
+		index++;
+	while (str[index] && ft_isdigit(str[index]))
+	{
+		number = 10.0 * number + (str[index] - '0');
+		index++;
+		decimal_count++;
+	}
+	return (sign * number * pow(0.1, decimal_count));
+}
