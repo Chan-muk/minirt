37a039c3bb5dba031ac6d2fdadb383f92d99833d
Myeongjin Go <mygo@c3r1s5.42seoul.kr>
2023-06-16 15:55:01 +0900

    UPDATE: phong_lighting fin

--- src/phong.c
+++ src/phong.c
@@ -1,5 +1,11 @@
 #include "minirt.h"
 
+t_vector	reflect(t_vector v, t_vector n)
+{
+	return (vec_sub(v, vec_mul(n, vec_dot(v, n) * 2)));
+}
+
+
 t_color		phong_lighting(t_scene *scene)
 {
 	t_color		light_color;
@@ -23,14 +29,40 @@ t_color		phong_lighting(t_scene *scene)
 	return (vec_cmp(vec_mul_(light_color, scene->rec.albedo), new_color(1, 1, 1)));	
 }
 
+// t_color		point_light_get(t_scene *scene, t_hit_array *light)
+// {
+// 	t_color		diffuse;
+// 	t_vector	light_dir;
+// 	double		kd;
+
+// 	light_dir = unit_vec(vec_sub(light->center, scene->rec.p));
+// 	kd = fmax(vec_dot(scene->rec.normal, light_dir), 0.0);
+// 	diffuse = vec_mul(light->light_color, kd);
+// 	return (diffuse);
+// }
+
 t_color		point_light_get(t_scene *scene, t_hit_array *light)
 {
-	t_color		diffuse;
-	t_vector	light_dir;
+	t_color	diffuse;
+	t_vector		light_dir;
 	double		kd;
+	t_color	specular;
+	t_vector		view_dir;
+	t_vector		reflect_dir;
+	double		spec;
+	double		ksn;
+	double		ks;
+	double		brightness;
 
 	light_dir = unit_vec(vec_sub(light->center, scene->rec.p));
-	kd = fmax(vec_dot(scene->rec.normal, light_dir), 0.0);
+	kd = fmax(vec_dot(scene->rec.normal, light_dir), 0.0);// diffuse strength;
 	diffuse = vec_mul(light->light_color, kd);
-	return (diffuse);
+	view_dir = unit_vec(vec_mul(scene->ray.dir, -1));
+	reflect_dir = reflect(vec_mul(light_dir, -1), scene->rec.normal);
+	ksn = 64; // shininess value
+	ks = 0.5; // specular strength;
+	spec = pow(fmax(vec_dot(view_dir, reflect_dir), 0.0), ksn);
+	specular = vec_mul(vec_mul(light->light_color, ks), spec);
+	brightness = light->bright_ratio * 3; // 기준 광속/광량을 정의한 매크로
+	return (vec_mul(vec_add(vec_add(scene->ambient, diffuse), specular), brightness));
 }
