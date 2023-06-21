944a1077b9839cf3b427e61539d46c3a732cb0e3
Myeongjin Go <mygo@c3r1s5.42seoul.kr>
2023-06-21 13:42:50 +0900

    FIX: fit norm bump.c

--- src/bump.c
+++ src/bump.c
@@ -1,63 +1,64 @@
 #include "minirt.h"
 
-t_vector bumprotateVector(t_vector normal, t_vector bump_vec)
+t_vector	bumprotatevector(t_vector normal, t_vector bump)
 {
-	t_vector standard = {0, 0, 1};
-	if (normal.x == standard.x && normal.y == standard.y && normal.z == standard.z)
-		return (unit_vec(bump_vec));
-	normal = unit_vec(normal);
-	bump_vec = unit_vec(bump_vec);
+	const t_vector	stdvec = {0, 0, 1};
+	const double	theta = acos(vec_dot(stdvec, unit_vec(normal)));
+	const t_vector	axis = unit_vec(vec_prod(stdvec, unit_vec(normal)));
+	double			rot[3][3];
+	t_vector		rotvec;
 
-	// Calculate rotation axis vector
-	t_vector axis = vec_prod(standard, normal);
-	axis = unit_vec(axis);
-
-	// Calculate the angle between the two vectors
-	double theta = acos(vec_dot(standard, normal));
-
-	// Calculate the rotation matrix
-	double rotationMatrix[3][3] = {
-		{cos(theta) + axis.x * axis.x * (1 - cos(theta)), axis.x * axis.y * (1 - cos(theta)) - axis.z * sin(theta), axis.x * axis.z * (1 - cos(theta)) + axis.y * sin(theta)},
-		{axis.y * axis.x * (1 - cos(theta)) + axis.z * sin(theta), cos(theta) + axis.y * axis.y * (1 - cos(theta)), axis.y * axis.z * (1 - cos(theta)) - axis.x * sin(theta)},
-		{axis.z * axis.x * (1 - cos(theta)) - axis.y * sin(theta), axis.z * axis.y * (1 - cos(theta)) + axis.x * sin(theta), cos(theta) + axis.z * axis.z * (1 - cos(theta))}};
-
-	// Calculate the rotated vector
-	t_vector rotatedVector;
-	rotatedVector.x = rotationMatrix[0][0] * bump_vec.x + rotationMatrix[0][1] * bump_vec.y + rotationMatrix[0][2] * bump_vec.z;
-	rotatedVector.y = rotationMatrix[1][0] * bump_vec.x + rotationMatrix[1][1] * bump_vec.y + rotationMatrix[1][2] * bump_vec.z;
-	rotatedVector.z = rotationMatrix[2][0] * bump_vec.x + rotationMatrix[2][1] * bump_vec.y + rotationMatrix[2][2] * bump_vec.z;
-
-	return rotatedVector;
+	if (normal.x == stdvec.x && normal.y == stdvec.y && normal.z == stdvec.z)
+		return (unit_vec(bump));
+	bump = unit_vec(bump);
+	rot[0][0] = cos(theta) + axis.x * axis.x * (1 - cos(theta));
+	rot[0][1] = axis.x * axis.y * (1 - cos(theta)) - axis.z * sin(theta);
+	rot[0][2] = axis.x * axis.z * (1 - cos(theta)) + axis.y * sin(theta);
+	rot[1][0] = axis.y * axis.x * (1 - cos(theta)) + axis.z * sin(theta);
+	rot[1][1] = cos(theta) + axis.y * axis.y * (1 - cos(theta));
+	rot[1][2] = axis.y * axis.z * (1 - cos(theta)) - axis.x * sin(theta);
+	rot[2][0] = axis.z * axis.x * (1 - cos(theta)) - axis.y * sin(theta);
+	rot[2][1] = axis.z * axis.y * (1 - cos(theta)) + axis.x * sin(theta);
+	rot[2][2] = cos(theta) + axis.z * axis.z * (1 - cos(theta));
+	rotvec.x = rot[0][0] * bump.x + rot[0][1] * bump.y + rot[0][2] * bump.z;
+	rotvec.y = rot[1][0] * bump.x + rot[1][1] * bump.y + rot[1][2] * bump.z;
+	rotvec.z = rot[2][0] * bump.x + rot[2][1] * bump.y + rot[2][2] * bump.z;
+	return (rotvec);
 }
 
-t_vector	shpere_bumpvec(t_vector p, t_hit_array *sp, t_hit_record* rec)
+t_vector	shpere_bump(t_vector p, t_hit_array *sp, t_hit_record *rec)
 {
-	double	u;
-	double	v;
+	double			u;
+	double			v;
+	int				i;
+	unsigned char	*addr;
+	t_vector		bump;
 
 	u = (rec->normal.x + 1) * 0.5;
 	v = (rec->normal.y + 1) * 0.5;
-	int	x = (int)(sp->bump_w * u);
-	int	y = (int)(sp->bump_h * v);
-	int	index = (x + y * sp->bump_w) * 3;
-	t_vector bump = new_vec(sp->bump_addr[index + 2] / 255.0, sp->bump_addr[index + 1] / 255.0, sp->bump_addr[index] / 255.0);
+	i = ((int)(sp->bump_w * u) + (int)(sp->bump_h * v) * sp->bump_w) * 3;
+	addr = sp->bump_addr;
+	bump = new_vec(addr[i + 2] / 255.0, addr[i + 1] / 255.0, addr[i] / 255.0);
 	bump = new_vec((bump.x - 0.5) * 2, (bump.y - 0.5) * 2, (bump.z - 0.5) * 2);
-	rec->normal = bumprotateVector(rec->normal, bump);
+	rec->normal = bumprotatevector(rec->normal, bump);
 	return (rec->normal);
 }
 
-t_vector	plane_bumpvec(t_vector p, t_hit_array *pl, t_hit_record* rec)
+t_vector	plane_bump(t_vector p, t_hit_array *pl, t_hit_record *rec)
 {
-	double	u;
-	double	v;
+	double			u;
+	double			v;
+	int				i;
+	unsigned char	*addr;
+	t_vector		bump;
 
 	u = fract(p.x * 0.2);
 	v = fract(p.y * 0.2);
-	int	x = (int)(pl->texture_w * u);
-	int	y = (int)(pl->texture_h * v);
-	int	index = (x + y * pl->texture_w) * 3;
-	t_vector bump = new_vec(pl->bump_addr[index + 2] / 255.0, pl->bump_addr[index + 1] / 255.0, pl->bump_addr[index] / 255.0);
+	i = \
+	((int)(pl->texture_w * u) + (int)(pl->texture_h * v) * pl->texture_w) * 3;
+	addr = pl->bump_addr;
+	bump = new_vec(addr[i + 2] / 255.0, addr[i + 1] / 255.0, addr[i] / 255.0);
 	bump = new_vec((bump.x - 0.5) * 2, (bump.y - 0.5) * 2, (bump.z - 0.5) * 2);
-	rec->normal = bumprotateVector(rec->normal, bump);
+	rec->normal = bumprotatevector(rec->normal, bump);
 	return (rec->normal);
 }
